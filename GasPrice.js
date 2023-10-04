51f0404d61415307f09d062ca6b52f619ff753b7
gokhan <0xgokhan@pm.me>
2023-10-05 00:26:33 +0300

    gas 2

--- GasPrice.js
+++ GasPrice.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './GasPrice.css'; // Import your CSS
-
 const GasPrice = () => {
-  const [gasPrice, setGasPrice] = useState(null);
-
-  useEffect(() => {
-    const fetchGasPrice = async () => {
-      const response = await fetch(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`);
-      const data = await response.json();
-      setGasPrice(data.result.SafeGasPrice); // Adjust depending on the API response
-    };
-
-    fetchGasPrice();
-    const intervalId = setInterval(fetchGasPrice, 60000); // Update every 60 seconds
-
-    return () => clearInterval(intervalId); // Clear interval on component unmount
-  }, []);
-
-  return (
-    <div className="gas-price">
-      {gasPrice ? `Gas Price: ${gasPrice} Gwei` : 'Loading...'}
-    </div>
-  );
-};
-
-export default GasPrice;
+    const [gasPrice, setGasPrice] = useState(null);
+  
+    useEffect(() => {
+      const fetchGasPrice = async () => {
+        try {
+          const response = await fetch(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${process.env.REACT_APP_ETHERSCAN_API_KEY}`);
+          const data = await response.json();
+          setGasPrice(data.result.SafeGasPrice);
+        } catch (error) {
+          console.error("Failed to fetch gas price:", error);
+        }
+      };
+  
+      fetchGasPrice();
+      const intervalId = setInterval(fetchGasPrice, 60000); 
+  
+      return () => clearInterval(intervalId);
+    }, []);
+  
+    return (
+      <div className="gas-price">
+        {gasPrice ? `${gasPrice} Gwei` : 'Loading...'}
+      </div>
+    );
+  };
+  
