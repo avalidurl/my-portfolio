cdf3132d9a6acfaed40d2b980395ea47fd4ccb66
gokhan <0xgokhan@pm.me>
2023-10-17 02:28:18 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -15,6 +15,31 @@ function App() {
           <div className="links">
             <a href="https://twitter.com/0xgokhan" className="link-item twitter">Twitter</a>
             <a href="https://github.com/avalidurl" className="link-item github">Github</a>
+            <a href="https://warpcast.com/gokhan" className="link-item farcaster">Farcaster</a>
+            <a href="https://hey.xyz/u/acceleration" className="link-item lens">Lens</a>
+            <a href="https://bsky.app/profile/0xgokhan.bsky.social" className="link-item bluesky">Bluesky</a>
+            <a href="https://cryptodon.lol/@gokhan" className="link-item cryptodon">Mastodon</a>
+          </div>
+        </section>
+        <section className="link-section">
+          <h2>Writing</h2>
+          <div className="links">
+            <a href="https://gokhan.substack.com" className="link-item substack">Substack</a>
+            <a href="https://gokhan.mirror.xyz" className="link-item mirror">Mirror</a>
+            <a href="https://paragraph.xyz/@gokhan" className="link-item paragraph">Paragraph</a>
+            <a href="https://medium.com/@0xgokhan" className="link-item medium">Medium</a>
+            <a href="https://beta.cent.co/goekhanturhan/" className="link-item cent">Cent</a>
+            <a href="https://midnight.pub/site" className="link-item pub">Midnight Pub</a>
+          </div>
+        </section>
+        <section className="link-section">
+          <h2>Formality</h2>
+          <div className="links">
+            <a href="https://www.linkedin.com/in/goekhanturhan/" className="link-item linkedin">LinkedIn</a>
+            <a href="https://independentresearcher.academia.edu/gokhanturhan" className="link-item academia">Academia</a>
+            <a href="https://scholar.google.com/citations?user=EgZE838AAAAJ&hl=en" className="link-item scholar">Google Scholar</a>
+            <a href="https://www.researchgate.net/profile/Goekhan-Turhan-2" className="link-item gate">Research Gate</a>
+            <a href="https://hcommons.org/members/gokhanturhan/" className="link-item commons">Humanities Commons</a>
           </div>
         </section>
         <section className="link-section">
@@ -27,7 +52,7 @@ function App() {
             <a href="https://t.me/farcaster_istanbul" className="link-item tgfarcasteristanbul">TG: Farcaster Istanbul</a>
             <a href="https://t.me/+sCGdzUEACdVmYjk0" className="link-item tgdevcoffee">TG: Devcoffee</a>
             <a href="https://t.me/writingnfts" className="link-item tgwritingnfts">TG: Writing NFTs</a>
-            <a href="https://matrix.to/#/#art-in-finance:matrix.org" className='link-item matrix'>Matrix: Art in Finance</a>
+            <a href="https://matrix.to/#/#art-in-finance:matrix.org" className="link-item matrix">Matrix: Art in Finance</a>
           </div>
         </section>
         <section className="link-section">
